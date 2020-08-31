3428ec49dbd6efdcf1faf2793570ff20454ae295
AnupMahat <mahatanup8@gmail.com>
2020-08-31 14:22:29 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React, {useState, useEffect} from "react"
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent
+} from "@material-ui/core"
+import InfoBox from "./InfoBox"
+import Map from "./Map"
+import Table from "./Table"
+import {sortData} from "./util"
+import LineGraph from "./LineGraph"
+import "leaflet/dist/leaflet.css"
+
+
+import './App.css';
+
+
+function App() {
+  const [countries, setCountries] =useState([])
+  const [country, setCountry]= useState("worldwide")
+  const [countryInfo, setCounrtyInfo] = useState({})
+  const [tableData, setTableData] = useState([])
+  const [casesType, setCasesType] = useState("cases");
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([])
+
+  useEffect(()=>{
+    fetch ("https://disease.sh/v3/covid-19/all")
+    .then(response=> response.json())
+    .then(data=>{
+      setCounrtyInfo(data)
+    })
+  },[])
+  
+  useEffect(()=>{
+
+    const getCountriesData = async ()=>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=> response.json())
+      .then((data)=>{
+        const countries = data.map((country)=>({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }))
+          const sortedData= sortData(data)
+          setTableData(sortedData)
+          setMapCountries(data)
+          setCountries(countries)
+      })
+    }
+    getCountriesData()
+  },[])
+
+  const onCountryChange = async (event) =>{
+    const countryCode = event.target.value
+    const url = countryCode === "worldwide"
+      ?"https://disease.sh/v3/covid-19/all"
+      :`https://disease.sh/v3/covid-19/countries/${countryCode}`
+     await fetch(url)
+     .then(response=>response.json())
+     .then(data=>{
+       console.log(data)
+       setCountry(countryCode)
+        setCounrtyInfo(data)
+        countryCode === "worldwide"
+        ? setMapCenter({ lat: 34.80746, lng: -40.4796 })
+        :setMapCenter([data.countryInfo.lat,data.countryInfo.long])
+        setMapZoom(4)
+     }) 
+
+   
+  }
+
+  return (
+    <div className="app" > 
+      <div className="app__left">
+      <div className="app__header">
+      {/* Header*/}
+      {/* Title + Select input dropdoen field*/}
+      <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            varient="outlined"
+            value={country}
+            onChange={onCountryChange}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {
+              countries.map(country =>(
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+            {/*loop through all the countries and 
+            show a drop down*/}
+            
+          </Select>
+      </FormControl>
+    </div>
+
+    <div className="app__stats">
+      <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+      <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+      <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+            {/* IndoBoxes* title="coronavirus cases"/}
+            {/* IndoBoxes* title="coronavirus recoveries"/}
+            {/* IndoBoxes* title="coronavirus cases"*/}
+    </div>
+
+      {/* Map*/}
+      <Map
+      countries={mapCountries} 
+      center={mapCenter}
+      zoom={mapZoom}
+      />
+      </div>
+      
+      <Card className="app__right">
+        <CardContent>
+          <h3>live Cases by Country</h3>
+            <Table countries={tableData}/>
+            <h3>Worldwide New Cases</h3>
+
+            <LineGraph casesType={casesType} />
+            {/* Graph*/}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
